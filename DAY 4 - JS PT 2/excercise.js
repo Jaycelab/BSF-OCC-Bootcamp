c3add62b5fb15ba56d5423db332d17228f3425a9
jaycelab <jayxalvarado@gmail.com>
2024-02-26 21:50:35 -0800

    worked on day 4 js

---
+++ DAY 4 - JS PT 2/excercise.js
@@ -0,0 +1,14 @@
+let num1 = 3;
+let num2 =10;
+
+checkEvenOrOdd = (num) => {
+	if (num % 2 ===0 ) {
+		console.log(`The number ${num} is even`)
+	}
+	else {
+		console.log(`The number ${num} is odd`)
+	}
+}
+
+checkEvenOrOdd(num1)
+checkEvenOrOdd(num2)
